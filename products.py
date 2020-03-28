2e15d35cd143bc9a634acd96cea0b1793c59a9f8
tzchung pan <louis8598@gmail.com>
2020-03-29 00:33:19 +0800

    增加一段語法，跳過標題列，繼續下一行

--- products.py
+++ products.py
@@ -2,14 +2,15 @@
 #讀取檔案 #就是先讀取檔案後，把讀取的內容先裝進清單
 #後續輸入新的內容，才不會全部洗掉，這就是讀取的作用
 products = [] #將product抓上來當清單
-with open('記帳本.csv', 'r') as f: #不同作業系統會有編碼問題，建議還是要加入編碼utf-8，可是如果是中文作業系統加入這個可能會出現錯誤代碼
+with open('記帳本.csv', 'r', encoding='utf-8') as f: #不同作業系統會有編碼問題，建議還是要加入編碼utf-8，可是如果是中文作業系統加入這個可能會出現錯誤代碼
     for line in f:
+        if '日期,商品名稱,價格' in line: #不要讓標題重複寫入所以加入這行
+            continue #跟break一樣，只是contiue是跳過上面說的東西，繼續下一行
         date, name, price  = line.strip().split(',')
         #記得要先用strip將\n或空格去掉，不然csv會出現在\n在裡面，
         #然後再用split切割檔案，並利用逗號做為區隔，這個會有一個順序(由左而右)
         #先在新增功能，與下面一樣，不要重複寫入，而是將data,name,price三個已知名稱分別寫入，也是一個簡寫法
-        products.append([date, name, price]) #讀取之後，要把他加入product裡面，所以用跟下面一樣的寫法就可以
-print(s)
+        products.append([date, name, price])  #讀取之後，要把他加入product裡面，所以用跟下面一樣的寫法就可以
 
 while True:
     date = input('請輸入日期：')
